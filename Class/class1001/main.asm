a212414b178ac22a7d96c5e008e206d30a05286c
Shahaf Dan <dan.shachaf@gmail.com>
2019-10-01 12:16:49 -0700

    first Stack Code

--- Class/class1001/main.asm
+++ Class/class1001/main.asm
@@ -7,9 +7,11 @@
 %include "./functions64.inc"
  
 SECTION .data
-	; put your variables here
+	; put your variables below
 	welcomeAct db "Hello World", 0ah, 0dh, 0h
 	goodbyeAct db "Bye, have  good one", 0ah, 0dh, 0h
+	
+	
 	
 SECTION .bss
 	;reserve memory here
@@ -19,10 +21,34 @@ SECTION     .text
      
 _start:
 	;code goes here
+	;---------HELLO---------
 	push welcomeAct
 	call PrintString 
 	call Printendl
 	
+	;----------STACK CODE --------
+	push 10h ;push to stack
+	push 20h ;push to stack
+	push 30h ;push to stack
+	
+	pop rax
+	push rax
+	call Print64bitNumHex ;print the register we pushed
+	call Printendl
+	
+	pop rax
+	push rax
+	call Print64bitNumHex ;print rax again
+	call Printendl
+	
+	pop rax
+	push rax
+	call Print64bitNumHex ;print rax again
+	call Printendl
+	
+	
+	;------GOODBYTE----
+	call Printendl
 	push goodbyeAct
 	call PrintString
 	call Printendl

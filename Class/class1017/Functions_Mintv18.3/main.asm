4334db9ee18e63368613564f21f2a9a887eb76b1
Shahaf Dan <dan.shachaf@gmail.com>
2019-10-17 13:06:30 -0700

    finished class assignment

--- Class/class1017/Functions_Mintv18.3/main.asm
+++ Class/class1017/Functions_Mintv18.3/main.asm
@@ -24,30 +24,63 @@ _start:
 	call Printendl
 	
 	nop
+	;jump to L1 if either bit 4,5 or 6 is set in theBL register
+	and bl, 00111000b
+	cmp bl, 0
+		jne L1
 	
-	cmp BYTE [anInteger], 50h
-	je theirEqual
-	jae itsEitherGreaterThanOrEqual
-	jbe itsEitherLessThanOrEqual
-	ja isGreaterThan
-	jb itsLessThan
+	;jump to L2 if bits 4,5,6 are all set in the BL register
+	and bl, 00111000b
+	cmp bl, 00110000b ;equivalent to 3h
+		je L2
+	
+	
+	;jump to L3 if Al has the even prity flag (if there was amathematical operation active on it)
+	add al, 0
+		jp L3
+	
+	;jump to L4 if eax is negative
+	and eax, 8000h
+	cmp eax, 8000h
+		je L4
+	;another way to do it:
+	add eax, 0
+		js L4
+		
+	;jump to L5 if ebx - ecx is greater than zero
+	mov edx, ebx
+	sub edx, ecx
+	cmp edx, 0h
+		jae L5
+		
+	;last resort
 	jmp goesHere
 	
-	goesHere: ;should never happen
+	goesHere: ;last resoort
 		nop
 		jmp endit
 	
-	theirEqual: 
+	L1:
 		nop
-		;do something because the ineteger wasequal to 50h
+		;do something becase an integer was less than 50h
 		jmp endit
 		
-	isGreaterThan:
+	L2:
+		nop
+		;do something becase an integer was less than 50h
+		jmp endit
+		
+	L3:
+		nop
+		;do something becase an integer was less than 50h
+		jmp endit
+		
+	L4:
 		nop
 		;do something becase an integer was greater than 50h
 		jmp endit
 		
-	itsLessThan:
+	L5:
 		nop
 		;do something becase an integer was less than 50h
 		jmp endit

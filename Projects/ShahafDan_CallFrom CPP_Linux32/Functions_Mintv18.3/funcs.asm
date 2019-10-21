5fdfc00d78c79f8efc276900ba70e07d47496d7b
Shahaf Dan <dan.shachaf@gmail.com>
2019-10-21 10:56:34 -0700

    started functions

--- Projects/ShahafDan_CallFrom CPP_Linux32/Functions_Mintv18.3/funcs.asm
+++ Projects/ShahafDan_CallFrom CPP_Linux32/Functions_Mintv18.3/funcs.asm
@@ -4,10 +4,16 @@ SECTION .data
 	stdin db 0h
 	sys_read db 03h
 
+
 global DisplayText
 global Displayendl
+global revArray
 global ReadText
-	
+global addTwoArrays
+global addTwo
+global multiplyTwo
+global pow2
+global addArray
 
 DisplayText:
 	mov eax, 04h ;we are goin to write something
@@ -30,3 +36,47 @@ ReadText:
 	mov eax, sys_read ;what are we going to read from
 	int 80h ;poke the kernel
 ret
+
+revArray:
+	;put functions code here
+	mov ebx, [esp + 4] ;mov to ebx the pointer to the first array
+	mov ecx, [esp + 8] ;mov to ecx the pointer to the second array
+ret
+
+addTwoArrays:
+ret
+
+addTwo:
+ret
+
+multiplyTwo:
+ret
+
+pow2:
+	mov esi, [esp + 4] ;copies the value from the stack to esi
+	mov eax, esi ;will return eax later at ret
+	mov ecx, esi ;multiple eax by esi, esi times
+	PowLoop:
+		mul eax, esi ;multiple eax by esi
+	Loop PowLoop ;traverse through the loop
+	
+	mov esi, 0 ;clean pointers, just in case
+	mov edi, 0 ;clean pointers just in case
+ret ;always returns eax
+	
+addArray
+	;taken from the presentation
+	nop 
+	mov esi, [esp + 4] ;copy the first parameter to esi; pointer to an array
+	mov ecx, [esp + 8]; copy second parameter to ecx; number of items in the array
+	push ebx ;needed when c++
+	mov ebx, 0 ;set counter to zero
+	mov eax, 0 ;set total to zero
+	
+	Loop1:
+		mov edx. [esi + ebx] ;copy values in edx to eax
+		add eax, edx ;add the array value in edx to eax
+		add ebx, 4 ;incremeny ebx by a double word size (4 bytes)
+	loop Loop1 ;ecx contrains the counter - decrementit and loop
+	pop ebx ;needed when C++ calls assembler programs
+ret ;return

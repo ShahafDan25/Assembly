a0bf856907a362de4f2ab483caa47fd0a968912b
Shahaf Dan <dan.shachaf@gmail.com>
2019-10-29 13:32:49 -0700

    finished 64 bit app

--- Class/class1029/main.asm
+++ Class/class1029/main.asm
@@ -10,6 +10,11 @@ SECTION .data
 	
 	welcomeAct	db	"Welcome to my progrm", 0ah, 0dh, 0h
 	byeAct db 	"Bye, have a good day!", 0ah , 0dh, 0h
+	pA db "Process A", 0ah, 0dh, 0h
+	pB db "Process B", 0ah, 0dh, 0h
+	pC db "Process C", 0ah, 0dh, 0h
+	pD db "Process D", 0ah, 0dh, 0h
+	pDefault db "Default Process", 0ah, 0dh, 0h
 	
 	CaseTable	db 'A'
 				dq Process_A
@@ -38,19 +43,23 @@ _start:
 	call Printendl ;print empty line
 	
 	;Process the Sqitch statment / case table
-	mov rcx, caseTable ;number of items in the switch
-	mov rsi, caseTable ;put the address of our table into rsi
-	mov al, 'C' ;our test value
+	mov rcx, CaseTable ;number of items in the switch
+	mov rsi, CaseTable ;put the address of our table into rsi
+	mov al, 'D' ;our test value
 	
 	Switch1:
 		cmp al, [rsi] ;compare our value to the lookup table value
-		jne Switch1_goAgain
+		jne Switch1_goAgain ; This is not the one
 		call NEAR [rsi + 1] ; call the function associated with the found value
+		;nesr is used to tell the computer we are using an address, not a value
 		jmp leave_Switch1 ;flag to leave the loop
 		
-	Switch_goAgain: ;go to the next one
-		add rsi, CaseTable.entrySize
+		Switch1_goAgain: ;go to the next one
+			add rsi, CaseTable.entrySize
+			
 	Loop Switch1
+		call Switch_default
+	
 	
 	leave_Switch1: ;a flag to be called to get out of the loop ;equivalent to break; from a loop in c++
 	;----- GOODBYE ---
@@ -66,14 +75,24 @@ Exit:
 	syscall							;Poke the kernel
 
 Process_A:
-
+	push pA
+	call PrintString
 ret ;finish Process_A
 Process_B:
-
+	push pB
+	call PrintString
 ret ;finish Process_B
 Process_C:
-
+	push pC
+	call PrintString
 ret ;finish Process_C
 Process_D:
-
+	push pD
+	call PrintString
 ret ;finish Process_D
+Switch_default: ;default condition ;Beleit Breyrah
+	push pDefault
+	call PrintString
+	Loop Switch1
+	mov rbx, 9999h
+ret

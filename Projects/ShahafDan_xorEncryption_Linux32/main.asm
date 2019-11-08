cced6bacf7fc008a6841a6a96b1c00d48353cf1e
Shahaf Dan <dan.shachaf@gmail.com>
2019-11-08 15:25:59 -0800

    fixed length of encryption bug

--- Projects/ShahafDan_xorEncryption_Linux32/main.asm
+++ Projects/ShahafDan_xorEncryption_Linux32/main.asm
@@ -86,41 +86,34 @@ _start:
 	push menu;
 	call PrintString
 	
+	;----------SET UP --------
 	mov ecx, caseTable ;mov the number of items in the switch ;set counter for the switch loop
 	mov esi, caseTable ;put the address of our table into the pointer esi register
-	
 	push indexValue
 	push indexValue.len
 	call ReadText ;;read input from user
 	mov al, [indexValue] ;move to the al 1 bytes register the input from the user
 	;call ClearKBuffer
 	
-	
+	;---------SWITCH ---------
 	Switch: ;create a loop to go through every element of the table
-	
 		cmp al,'x' ;compare to x, in order to exit the program
 		je exitFlag ;if the user entered x, jmp to exitFlag (exit the program)
-	
 		cmp al, [esi] ;compre to the look up table
 		jne nextSwitchItem ;try the next value in the switch table
 		call NEAR[esi + 1] ;call the function associated ith the user input
-		
 		jmp printMenu
-		
 		nextSwitchItem:
 			add esi, caseTable.entrySize
-			cmp esi, caseTable + (caseTable.numberOfEntries * caseTable.entrySize)
-			jge notFoundInTable
-				
+			cmp esi, caseTable + (caseTable.numberOfEntries * caseTable.entrySize) ;compare addresses, of greater than max, then the value entered by the user is not in the table driven selection, display the "invliad" message
+			jge notFoundInTable		
 	Loop Switch ; go to the switch flag again
-	
-	
+	;---------INVLIAD INPUT ---------
 	notFoundInTable: ;flag to jump to if the value the user entered cannot be found in the look up table
 		push notFound
 		call PrintString
 		call Printendl
 		jmp printMenu ;go back to printing the menu again
-		
 	;----- GOODBYE ---
 	exitFlag:
 	push byeAct
@@ -128,7 +121,7 @@ _start:
 	call Printendl
 	
 ;
-;Setup the registers for exit and poke the kernel
+;----------- EXIT -------------
 Exit: 
 	mov		eax,sys_exit				;What are we going to do? Exit!
 	mov		ebx,0						;Return code
@@ -141,27 +134,21 @@ enterString:
 	;mov [inputString], 0h
 	push option1
 	call PrintString ;print prompt
-	
 	push inputString
 	push inputString.len
 	call ReadText ; returns in eax the amount of characters	
-	
 	mov [inputStringSize], eax
 	mov eax, 0
-	
 ret
 
 enterKey:
-	mov eax, 0
-	;mov [inputKey], 0
-	push option2
+	mov eax, 0 ; clear just in case
+	push option2 
 	call PrintString
-	
 	push inputKey
 	push inputKey.len
-	call ReadText
-	;call ClearKBuffer
-	mov [inputKeySize], eax
+	call ReadText ;get input from the user
+	mov [inputKeySize], eax ; ReadText returns the "actual" length in eax, move it to a variable
 	mov eax, 0
 ret
 	
@@ -179,26 +166,22 @@ printKey: ;just printing promopt and the key
 	push inputKey
 	call PrintString
 	call Printendl
-
 ret
 
 ;----5----
 encryptString:
 	push option5 ;print the right prompt
 	call PrintString
-	
-	
 	mov ebx, 0
 	mov eax, 0
 	mov edx, 0
 	mov edi, 0
 	mov ecx, 0
-	mov ecx, [inputStringSize] ;same as the encryption loop
+	mov cl, [inputStringSize] ;same as the encryption loop
 	sub ecx, 1
 
 	encLoop:
 		;--- CODE FOR ENCRYPTING --- 
-		
 		mov eax, edi ;gotta move first to eax, because th div operand onlyt operates on the eax register
 		mov ebx, [inputKeySize]
 		div ebx ;stores the modules in edx
@@ -207,7 +190,6 @@ encryptString:
 		xor ebx, [inputKey + edx] ;xor with the same key
 		mov [encValue + edi], ebx
 		inc edi
-		
 	Loop encLoop
 	
 	;debug by printing
@@ -218,23 +200,22 @@ ret
 
 ;------6-----------
 decryptKey:
-	push option6
+	push option6 ;print promopt just for clearabce
 	call PrintString
 	mov ebx, 0
 	mov eax, 0
 	mov edx, 0
 	mov edi, 0
 	mov ecx, 0
-	mov ecx, [inputStringSize] ;same as the encryption loop
+	mov cl, [inputStringSize] ;same as the encryption loop
 	sub ecx, 1
 
 	decLoop:
 		;CODE FOR DECRYPTIONG GOES HERE
-
 		mov eax, edi ;gotta move first to eax, because th div operand onlyt operates on the eax register
 		mov ebx, [inputKeySize]
 		div ebx ;stores the modules in edx
-		mov ebx ,0
+		mov ebx ,0 ;clear before messing with it again
 		mov ebx, [encValue + edi]
 		xor ebx, [inputKey + edx] ;xor with the same key
 		mov [decValue + edi], ebx
@@ -251,7 +232,4 @@ ret
 ;-----TODO-------
 ;1) do not forget to clear buffer 
 ;2) reerase everytime user enters input
-;3)leave as many comments as possible
-;7) add option of chooising somerthing not in menu
-;8) how to display the invliad message
-;9) clean code to as less lines as possible
+

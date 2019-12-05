d23ff6b6ba3a22c19e91488bfb0433f9e672389e
Shahaf Dan <dan.shachaf@gmail.com>
2019-12-05 14:00:19 -0800

    finished class project

---
+++ Projects/ShahafDan_FloatingPoint_Linux32/main.asm
@@ -0,0 +1,119 @@
+;
+;This program will test out the functions library
+;
+;
+;Include our external functions library functions
+%include "./functions.inc"
+ 
+SECTION .data
+	welcomeAct	db	"Welcome to my program", 0ah, 0dh, 0h
+	byeAct 		db	"Have a cool day", 0ah, 0dh, 0h
+	
+	enterA		db 	"Enter A's Value please!", 0ah, 0dh, 0h
+	enterB		db 	"Enter B's Value please!", 0ah, 0dh, 0h
+	enterC		db 	"Enter C's Value please!", 0ah, 0dh, 0h
+	
+	prompt		db "((A+B) * (B / C)) ** 2  = ", 0h
+	
+SECTION .bss
+	
+	A resd 1
+	B resd 1
+	C resd 1
+	ApB 	resd	1
+	BdC		resd 	1
+	ABC		resd	1
+	D	resd 	1
+SECTION     .text
+	global      _start
+     
+_start:
+	
+	;============ WELCOME============
+	call Printendl;
+	push welcomeAct ;prepares to call welcomeAct
+	call PrintString ;prints the welcome act
+	call Printendl;prints an empty line
+	call Printendl
+	
+	;=============== ASSIGNMENT =============
+	nop
+	
+	;-----Print and read first
+	mov eax, 0
+	push enterA
+	call PrintString
+	call Printendl
+	call InputFloat				;stores input in eax
+	fstp DWORD [A]
+	
+	
+	
+	mov eax, 0
+	push enterB
+	call PrintString
+	call Printendl
+	call InputFloat				;stores input in eax
+	fstp DWORD [B]
+	
+	mov eax, 0
+	push enterC
+	call PrintString
+	call Printendl
+	call InputUInt				;stores input in eax
+	mov [C], eax
+	
+	fld dword [A]					;push A onto the stack
+	fld dword [B]					;push B onto to second spot in the stack
+	fadd							;add A + B
+	fstp dword [ApB]					;store A + B in ApB
+	
+	
+	
+	fld dword [B]					;St(0) = B
+	fild dword [C]					;ST(1) = C
+	fdiv 							;St(0) = ST0 / St1 = B / C
+	fstp dword [BdC]				;store St (0) in BdC
+	
+			
+	fld dword [ApB]					;put in ST0
+	fld dword [BdC]					;put in ST1
+	fmul 					;multiply the ApB and BdC
+	
+	fstp dword [ABC]
+	
+	
+	
+	fld dword [ABC]
+	fld dword [ABC]
+	fmul 	
+	
+						;Squaring it
+	
+	fstp dword	[D]				;store the final reult in the final reserved variable
+		
+		
+		
+		
+	push prompt
+	call PrintString
+	mov ebx, [D]
+	push ebx
+	push 8
+	call PrintDWFloat			;not sure about how to print it
+	call Printendl
+	
+	
+	nop
+	
+	;================= GOODBYE ===============
+	push byeAct ;print the goodbye statment
+	call PrintString
+	call Printendl
+	call Printendl
+	
+;Setup the registers for exit and poke the kernel
+Exit: 
+	mov		eax,sys_exit				;What are we going to do? Exit!
+	mov		ebx,0						;Return code
+	int		80h							;Poke the kernel

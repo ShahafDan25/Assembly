4bf6ccb48642d905393d5b87f6795d1cfc892e19
Shahaf Dan <dan.shachaf@gmail.com>
2019-12-05 21:44:14 -0800

    almost done with part 1

--- Projects/TakingAByteOut_ShahafDan_Linux64/main.asm
+++ Projects/TakingAByteOut_ShahafDan_Linux64/main.asm
@@ -14,25 +14,17 @@ SECTION .data
 	avgAct db "Average is: ", 0h
 	totalAct db "Total is: ", 0h
 	valuesAct db "the values given are: ", 0h
-	VarianceAct db "The Variance of these values is: ", 0h
+	VarianceAct db "The Variance is: ", 0h
 	PrintMinus db "-", 0h
 	
 	
-	
-	valuesArray	dq	-512, -3, 245, 800, -88 ;everything should be signed
-		.len equ (($ - valuesArray) /8 );divide by 8 because we are using quad word
-	;valuesArray	dq	-365, -722, 567, -876, -222 ;everything should be signed
+	;valuesArray	dq	-512, -3, 245, 800, -88 ;everything should be signed
 	;	.len equ (($ - valuesArray) /8 );divide by 8 because we are using quad word
+	valuesArray	dq	-365, -722, 567, -876, -222 ;everything should be signed
+		.len equ (($ - valuesArray) /8 );divide by 8 because we are using quad word
 		
-		
-	total	dq	0h; set total to zero, we will use total / length to calculate he average (mean)
-	average dq	0h
-	varianceTotal dq 0h ;store total of squared values previously to calculating variance
-	varianceAverage dq 0h
 SECTION .bss
-	;reserve memory here
-	tempArray resq valuesArray.len
-		.len equ (($ - tempArray) / 8);
+	
 	
 SECTION     .text
 	global  _start
@@ -56,63 +48,11 @@ _start:
 	
 	
 	;Call the calcvariance function
-	push tempArray
-	push valuesArray					;push into the stack frame	
-	;push valuesArray.len				;push into the stack frame
-
+	push valuesArray.len
+	push valuesArray
+						;push into the stack frame	
 	call calcvariance					;call the variable
-	
-	
-	;========= PRINT AND CALCULATE VALUES (also total loop =============
-
-	
-
-	;======= CALCULATING MEAN DIFFERENCES =============
-	mov rax, 0
-	mov rbx, 0
-	call Printendl
-	mov rcx, 0
-	mov rsi, 0 ;clearing registers, first of all
-	mov rcx, valuesArray.len;set iterator
-	MeanDifLoop:
-		; for each value:
-		;1) substract the previosuly calculated mean
-		;2) sqaure the new value
-		;3) add it to varianceTotal
-		mov rax, [valuesArray + rsi] ;move o rax the next value in line
-		clc
-		mov rbx, [total]
-		bt rbx, 63
-		jnc cont0
-		add rax, [average]
-		jmp cont9
-		cont0:
-		sub rax, [average]
-		cont9:
 		
-	
-		imul rax ;multiply rax by rax (squaring it)
-		add [varianceTotal], rax ;add the new values (which much be positive, becauseit was sqaures) to its corresponding variable
-		
-		mov rax, 0 ;clean rax for safety reasons LOL
-		mov rbx, 0 ;clean the average holder register
-		add rsi, 8;8 for quad words
-	loop MeanDifLoop
-		call Printendl
-
-
-;============= PRINT VARIANCE ===============
-push VarianceAct
-call PrintString
-	mov rcx, 0
-	mov rax, 0
-	mov rax, [varianceTotal]
-	mov cx, valuesArray.len
-	idiv rcx ;divide the variance toatl (storedin rax) by rcx
-	mov [varianceAverage], rax
-	
-	push rax
-	call Print64bitNumDecimal
 	
 	;=========== GOODBYE MESSAGE ================
 	call Printendl
@@ -135,10 +75,13 @@ calcvariance:
 	;=========CREATE THE STACK============
 	push rbp 						;Store the current stack frame
 	mov rbp, rsp					;Preserve esp into ebp for argument reference			
-	mov rbx, 0						;we will store the total in rbx for now
+	mov rbx, 0		
+	mov rdx, 0
+	mov rcx, 0				;we will store the total in rbx for now
 	;========= STACK ACTION CODE ============
 	mov rdi, [rbp + 16]				;push the address of the sampleArray stored in rbp + 16 to a pointer variable
-	mov rcx, 5						;Do we pass the number of elements as a parameter? - CHANGE
+	mov rcx, [rbp + 24];
+	
 	loopie:
 		mov rax, [rdi]				;move to rax the dereferenced value stored in the rdi pointer
 		clc							;clear carry flag, we will use it to detect negative values
@@ -151,11 +94,15 @@ calcvariance:
 		jmp neg
 		notNeg:						; conitnue code here if the value is not negative
 		add rbx, rax				;add the value to the total
-		neg:						;continue code here if thevalue IS negative
+		neg:	
+
 		push rax					;prepare to print
 		call Print64bitNumDecimal	;Print values
+		cmp rcx, 1
+		je lastOne
 		call PrintComma				;Print a coma
-		add rdi, 8 					;go to the next argument in the array, whose address is stored in the rdi pointer
+		lastOne:
+		add rdi, 8
 	loop loopie
 	
 	call Printendl					;line spacing
@@ -164,6 +111,7 @@ calcvariance:
 	;------ Print total----------
 	push totalAct					;Print total prompt
 	call PrintString				;Print it
+	mov rdx, rbx
 	clc								;Clear he carry flag: will be used to determine sign of total
 	bt rbx, 63						;get the SIGNficiant bit of the total (storedin rbx)
 	jnc notNeg2						;jump if not negative
@@ -179,15 +127,18 @@ calcvariance:
 	;----------Calculate average----------
 	;carry still holds the negation value!
 	mov rax, 0						;clear rax
-	mov rax, rbx
-	mov rcx, 5						;now rax holds the total
+	mov rax, rbx					;now rax holds the total
+	mov rcx, [rbp + 24]				;now rcx holds valuesArray.len
+	mov rbx, 0
+	mov rbx, rdx
+	cdq								;Expand to avoid floating point exception
 	idiv rcx						;divide rax with 5 - CHANGE TO A LENGTH VARIABLE
 	
 	push avgAct						;prepare to print average act
 	call PrintString				;print it
-	clc								;Clear carry
-	bt rax, 63						;check SIGNificant bit	
-	jnc notNeg3						;if the number is negative {
+	clc
+	bt rbx, 63					;check SIGNificant bit	
+	jnc notNeg3					;if the number is negative {
 	push PrintMinus					;print a minus
 	call PrintString				;}
 	notNeg3:						;else {...}	
@@ -195,46 +146,78 @@ calcvariance:
 	call Print64bitNumDecimal		;average <<
 	call Printendl					;endl;
 	
-	AVG equ 0h
-	mov AVG, rax
-	;------- CREATE SPACE FOR SQUARED VALUES ARRAY ----------
-	mov rcx, 5						;CREATE SIZE VARIABLE GODDAMNIT
-	addStackSpaceLoop:
-		sub rsp, 8		
-	loop addStackSpaceLoop
+	;sub rsp, 8						;create enough room for an average 
+	push rax						;rax (average(abs)) is now in the stack in RBP - 8 ;automatically does rsp - 8
+	mov rax, 0
 	
+	mov rsi, 0
+						;create enough room for a pointer for another array
+	;call Printendl
+	;push PrintMinus
+	;call PrintString
+	;call Printendl
 	;by now we should have x qw values in the stack
 	;--------- CALCULATE THE AVERAGE OF THE NEW ARRAY ----------
-	mov rdx, rax					;RDX now holds the values mean (RDX = AVERAGE)
-	mov rax, 0						;clear RAX register
-	mov rcx, 5						;Do we pass the number of elements as a parameter? - CHANGE
+	mov rdx, [rbp - 8]					;RDX now holds the values mean (RDX = AVERAGE)
+	mov rax, 0	
+	mov rdi, 0					;clear RAX register
+	mov rcx, [rbp + 24]						;Do we pass the number of elements as a parameter? - CHANGE
 	mov rdi, [rbp + 16]
 	varianceArrayLoop:
+		;sub rsp, 8					;create space for one more values in the array
 		mov rax, [rdi]				;move to rbx the dereferenced value stored in the rdi pointer
+		mov rdx, [rbp - 8]
+		clc
+		bt rax, 63
+		jnc	pos						;not a negative
+			neg rax
+			add rax, rdx				;if negaative, substract and take absolute value
+		jmp posCont
+		pos:
+			sub rax, rdx			;subtract the average in RDX from the current rax value
+		posCont:
 		
-		jnc	notNeg4					;not a negative average
-			add rax, rdx
-		jmp neg4
-		notNeg4:
-			sub rax, rdx
-		neg4:
-		imul rax					;Squar the edited value
-		push rax					;move the squared values into the next spot in the stack
-		call Print64bitNumDecimal
-		call PrintComma
+		imul rax					;Squar the edited value, does something to rdx everytime, so we have to reassign the value to rdx
+		sub rsp, 8					;create stack room for 8 more bits
+		mov QWORD [rsp], rax
+		
 		add rdi, 8 					;increase rdi (the iterator)
 		
 		mov rax, 0					;go to the next argument in the array, whose address is stored in the rdi pointer
 	loop varianceArrayLoop
-	mov rbx, 0
-	mov rdi, 0
-	mov rdi, [rbp + 24]				;move to rsi the address of the variable pushed first to the stack
-	mov rcx, 5						;size of array 
-	populateTempLoop:
-		
-		mov rbx, 0
-		add rdi, 8					;to go to the next quad word (8bytes) in the array, indexed way
-	loop populateTempLoop
+	
+	;----- CALCULATE AVERAGE OF SQUARED VALUES
+	mov rcx, 0
+	mov rcx, [rbp + 24]				;the number of elementsin the original array passed to the function
+	mov rax, 0						;RBX is going to hold our total
+	mov rdx, rsp
+	loopom:
+	add rax, [rdx]
+	
+	add rdx, 8
+			
+			
+			push rax
+			call Print64bitNumDecimal
+			call Printendl
+	loop loopom
+	
+	mov rcx, 0
+	mov rcx, [rbp + 24]
+	
+	push rax							;
+	call Print64bitNumDecimal
+	call Printendl
+	cdq									;Exapnd into RAX:RDX to avoid floating point exception
+	
+	idiv rcx
+	
+	push VarianceAct
+	call PrintString
+								;Divide by the number of elements
+	push rax							;
+	call Print64bitNumDecimal
+	call Printendl
 	;=========== DESTROY STACK ==============
 	mov rsp, rbp					;Restore the stack position
 	pop rbp							;Restore ebp's original value in the stack frame
@@ -246,7 +229,8 @@ ret
 
 
 
-
+;//// TODO
+;1 fix minus sign in the average printing
 
 
 

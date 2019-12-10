44bc679c2d847e8e84e4951d39d5bad6028a977b
Shahaf Dan <dan.shachaf@gmail.com>
2019-12-10 15:11:18 -0800

    finished memoery dynamic allocation

--- Class/class1205/main.asm
+++ Class/class1205/main.asm
@@ -7,6 +7,8 @@ section .data
 	floatVar1	dq	1.0
 	floatVar2	dq	2.0
 	floatVar3	dq	225.023
+	floatVar4	dq	32.99
+	floatVar5	dq	10.0
 	
 	
 section .bss
@@ -30,13 +32,28 @@ _start:
 	fmul										;floatVar2 * floatVar3
 	fld qword [floatVar1]						;push 1.0 onto the stack
 	fadd										;floatVar1 + (floatVar2 * floatVar3)
-	
+	fld qword [floatVar4]
+	fdiv
 	fstp qword [floatStore]						;store the result in the reserved variable
 	
 	fld qword [floatVar3]						;push 225.023
 	fsqrt										;sqaure root it
-	fld											; THIS WILL NOT take (ST(0)) and push it again to the FLU Stack
+	;fld										; THIS WILL NOT take (ST(0)) and push it again to the FLU Stack
 	
+		;fmul									;Multiply it byitself (should reserve the sqrt)
+	fmul qword [floatVar2]					
+	
+	
+	fcom qword [floatVar5]						;compare ST(0) with fv5 = 10
+	ja	ItsGreater								;If greater than 10- jump
+		nop										;It was less than 10
+		
+		
+	jmp exitit
+	ItsGreater:
+		nop
+	
+	exitit:
 	
 	
 	nop
